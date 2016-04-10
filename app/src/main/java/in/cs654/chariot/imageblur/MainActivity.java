f4229e3844cb723e9d73456c602a7045adaa89fb
Saurav Kumar <2020saurav@gmail.com>
2016-04-10 22:36:51 +0530

    integration with chariot service

--- app/src/main/java/in/cs654/chariot/imageblur/MainActivity.java
+++ app/src/main/java/in/cs654/chariot/imageblur/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,17 +16,26 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import in.cs654.chariot.avro.BasicRequest;
+import in.cs654.chariot.avro.BasicResponse;
+import in.cs654.chariot.utils.CommonUtils;
+import in.cs654.chariot.utils.PrashtiClient;
 
 public class MainActivity extends AppCompatActivity {
 
     int TAKE_PHOTO_CODE = 0;
     public static int count = 0;
     final static String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/chariot/";
+    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         File newdir = new File(dir);
         newdir.mkdirs();
@@ -49,7 +59,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText("Working...");
+                textView.setText("Waiting...");
+                try {
+                    // TODO correct it to call blur function (after DB and device setup)
+                    PrashtiClient client = new PrashtiClient("172.24.1.62");
+                    BasicRequest lifeUniv = BasicRequest.newBuilder()
+                            .setRequestId(CommonUtils.randomString(32))
+                            .setDeviceId("1")
+                            .setFunctionName("testFunc")
+                            .setArguments(new ArrayList<String>())
+                            .setExtraData(new HashMap<String, String>())
+                            .build();
+                    BasicResponse response = client.call(lifeUniv);
+                    textView.setText(response.getResponse().get("answer"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
